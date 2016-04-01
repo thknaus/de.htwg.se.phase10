52576dd60b54d20282702d997c05f5cb30d085b5
SiBorho <SiBorho@htwg-konstanz.de>
2016-04-01 18:04:53 +0200

    Add the basic information to the CardArray and the CardStack.

---
+++ de.htwg.phase10/src/de/htwg/phase10/CardStack.java
@@ -0,0 +1,20 @@
+package de.htwg.phase10;
+
+public class CardStack {
+	int size;
+	
+	
+	/*
+	 * get the last Card witch was discarded from the Stack to the Hand.
+	 */
+	public void getoldCard() {
+		
+	}
+	
+	/*
+	 * set a Card from the Hand to the Stack.
+	 */
+	public void setCardtoStack() {
+		
+	}
+}
--- de.htwg.phase10/src/de/htwg/phase10/Phase10Application.java
+++ de.htwg.phase10/src/de/htwg/phase10/Phase10Application.java
@@ -3,7 +3,6 @@ package de.htwg.phase10;
 public class Phase10Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo");
-		System.out.println("Richtig dumm");
+		System.out.println("Hier fehlt noch was");
 	}
 }
