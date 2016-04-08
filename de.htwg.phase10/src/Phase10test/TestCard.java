e1cff6ce97ccc668b2ca77ac146cef15570c5e7d
Thomas <thknaus@htwg-konstanz.de>
2016-04-08 11:47:26 +0200

    tested deck with class testcard

--- de.htwg.phase10/src/Phase10test/TestCard.java
+++ de.htwg.phase10/src/Phase10test/TestCard.java
@@ -1,15 +1,16 @@
 package Phase10test;
+import java.util.LinkedList;
+
 import de.htwg.phase10.*;
 import de.htwg.phase10.Card.Color;
 import de.htwg.phase10.Card.Rank;
 
 public class TestCard {
-	public static void main(String []args){
-		Card c = new Card(Rank.EIGHT, Color.BLUE);
-		Card b = new Card(Rank.NINE, Color.RED);
+	public static void main(String []args){		
+		Deck d = new Deck();
+		LinkedList<Card> cards = d.newDeck();
 		
-		System.out.println(c);
-		System.out.println(b);
+		System.out.println(cards);
 		
 	}
 }
