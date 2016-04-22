6a3db8366e3a42c778175637299ab5fb23089c28
thknaus <thknaus@htwg-konstanz.de>
2016-04-22 09:50:20 +0200

    Added hand

---
+++ de.htwg.phase10/src/de/htwg/se/phase10/phase10/hand.java
@@ -0,0 +1,19 @@
+package de.htwg.se.phase10.phase10;
+
+public class hand {
+
+	private int size;
+	private Card [] handcards;
+	public hand () {
+		handcards = new Card[10];
+		size = 10;
+	}
+	
+	public void newHand() {
+		for (int i = 0; i < 10; i++) {
+			
+		}
+	}
+	
+	
+}
