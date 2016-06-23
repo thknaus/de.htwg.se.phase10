e20db0bc630b8ac629960b81d70a36ab623ba399
SiBorho <SiBorho@htwg-konstanz.de>
2016-06-23 05:56:16 +0200

    divest the menubar

---
+++ de.htwg.phase10/src/de/htwg/se/phase10/aview/gui/Phase10MenuBar.java
@@ -0,0 +1,89 @@
+package de.htwg.se.phase10.aview.gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.Locale;
+
+public class Phase10MenuBar extends JMenuBar implements ActionListener{
+	JMenuBar menuBar;
+	JMenu menu, submenu; 
+	JRadioButtonMenuItem rbMenuItem;
+	JMenuItem quit;
+	
+	public Phase10MenuBar(){
+	//Build Menu menu.
+		menuBar = new JMenuBar();
+		
+		menu = new JMenu("Menu");
+		menu.setMnemonic(KeyEvent.VK_M);
+		menuBar.add(menu);
+					
+					
+		menu.addSeparator();
+		submenu = new JMenu("New Game");
+		submenu.setMnemonic(KeyEvent.VK_S);
+		menu.add(submenu);
+			 
+		JMenuItem numberofplayer = new JMenuItem("choose number of player");
+		submenu.add(numberofplayer);
+					
+		menu.addSeparator();
+		ButtonGroup group = new ButtonGroup();
+			        
+		rbMenuItem = new JRadioButtonMenuItem("2 Players");
+		rbMenuItem.setSelected(true);
+		rbMenuItem.setMnemonic(KeyEvent.VK_2);
+		group.add(rbMenuItem);
+		rbMenuItem.addActionListener(this);
+		submenu.add(rbMenuItem);
+			        
+		rbMenuItem = new JRadioButtonMenuItem("3 Players");
+		rbMenuItem.setSelected(true);
+		rbMenuItem.setMnemonic(KeyEvent.VK_3);
+		group.add(rbMenuItem);
+		rbMenuItem.addActionListener(this);
+		submenu.add(rbMenuItem);
+			        
+		rbMenuItem = new JRadioButtonMenuItem("4 Players");
+		rbMenuItem.setSelected(true);
+		rbMenuItem.setMnemonic(KeyEvent.VK_4);
+		group.add(rbMenuItem);
+		rbMenuItem.addActionListener(this);
+		submenu.add(rbMenuItem);
+			        
+		rbMenuItem = new JRadioButtonMenuItem("5 Players");
+		rbMenuItem.setSelected(false);
+		rbMenuItem.setMnemonic(KeyEvent.VK_5);
+		group.add(rbMenuItem);
+		rbMenuItem.addActionListener(this);
+		submenu.add(rbMenuItem);
+			        
+		rbMenuItem = new JRadioButtonMenuItem("6 Players");
+		rbMenuItem.setSelected(false);
+		rbMenuItem.setMnemonic(KeyEvent.VK_6);
+		group.add(rbMenuItem);
+		rbMenuItem.addActionListener(this);
+		submenu.add(rbMenuItem);
+			        
+		quit = new JMenuItem("Quit");
+		quit.setMnemonic(KeyEvent.VK_Q);
+		quit.addActionListener(this);
+		menu.add(quit);
+
+	}
+	//ActionEvent
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == quit) {
+			JOptionPane.setDefaultLocale(Locale.ENGLISH);
+			int n = JOptionPane.showConfirmDialog(this, "Quit Phase10?", "Close Phase10?", JOptionPane.YES_NO_OPTION);
+			if(n == JOptionPane.YES_OPTION){
+				System.exit(0);
+			} else if(n == JOptionPane.NO_OPTION){
+				return;
+			}
+		}
+	}
+}
--- de.htwg.phase10/src/de/htwg/se/phase10/aview/gui/Phase10gui.java
+++ de.htwg.phase10/src/de/htwg/se/phase10/aview/gui/Phase10gui.java
@@ -12,10 +12,7 @@ import java.util.Locale;
 
 public class Phase10gui extends JFrame implements ActionListener{
 	
-	JMenuBar menuBar;
-	JMenu menu, submenu; 
-	JRadioButtonMenuItem rbMenuItem;
-	JMenuItem quit;
+
 	JFrame field;
 	
 	
@@ -41,66 +38,7 @@ public class Phase10gui extends JFrame implements ActionListener{
 
 		//JPanel p = new JPanel(new BoarderLayout());
 
-//Build Menu menu.
-		menuBar = new JMenuBar();
-		this.setJMenuBar(menuBar);
-		
 
-		menu = new JMenu("Menu");
-		menu.setMnemonic(KeyEvent.VK_M);
-		menuBar.add(menu);
-		
-		
-		menu.addSeparator();
-        submenu = new JMenu("New Game");
-        submenu.setMnemonic(KeyEvent.VK_S);
-        menu.add(submenu);
- 
-		JMenuItem numberofplayer = new JMenuItem("choose number of player");
-		submenu.add(numberofplayer);
-		
-        menu.addSeparator();
-        ButtonGroup group = new ButtonGroup();
-        
-        rbMenuItem = new JRadioButtonMenuItem("2 Players");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_2);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(this);
-        submenu.add(rbMenuItem);
-        
-        rbMenuItem = new JRadioButtonMenuItem("3 Players");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_3);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(this);
-        submenu.add(rbMenuItem);
-        
-        rbMenuItem = new JRadioButtonMenuItem("4 Players");
-        rbMenuItem.setSelected(true);
-        rbMenuItem.setMnemonic(KeyEvent.VK_4);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(this);
-        submenu.add(rbMenuItem);
-        
-        rbMenuItem = new JRadioButtonMenuItem("5 Players");
-        rbMenuItem.setSelected(false);
-        rbMenuItem.setMnemonic(KeyEvent.VK_5);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(this);
-        submenu.add(rbMenuItem);
-        
-        rbMenuItem = new JRadioButtonMenuItem("6 Players");
-        rbMenuItem.setSelected(false);
-        rbMenuItem.setMnemonic(KeyEvent.VK_6);
-        group.add(rbMenuItem);
-        rbMenuItem.addActionListener(this);
-        submenu.add(rbMenuItem);
-        
-        quit = new JMenuItem("Quit");
-        quit.setMnemonic(KeyEvent.VK_Q);
-        quit.addActionListener(this);
-		menu.add(quit);
 
         
 //Hauptfenster ausgeben
@@ -108,24 +46,18 @@ public class Phase10gui extends JFrame implements ActionListener{
 		this.setVisible(true);
 	}
 	
-//ActionEvent
-	@Override
-	public void actionPerformed(ActionEvent e) {
-    	Object source = e.getSource();
-    	if(source == quit) {
-    		JOptionPane.setDefaultLocale(Locale.ENGLISH);
-    		int n = JOptionPane.showConfirmDialog(this, "Quit Phase10?", "Close Phase10?", JOptionPane.YES_NO_OPTION);
-    		if(n == JOptionPane.YES_OPTION){
-    			System.exit(0);
-    		} else if(n == JOptionPane.NO_OPTION){
-    			return;
-    		}
-    	}
 
-    }
 	
 //main	
     public static void main(String[] args) {
 		JFrame myApp = new Phase10gui();
     }
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
