3260b4862277cce2d6b5844b0b1d3881294c83fd
Thomas <thknaus@htwg-konstanz.de>
2016-06-30 19:28:26 +0200

    worked on tui test.

--- de.htwg.phase10/test/de/htwg/phase10/tui/TextUITest.java
+++ de.htwg.phase10/test/de/htwg/phase10/tui/TextUITest.java
@@ -9,6 +9,7 @@ import com.google.inject.Injector;
 import de.htwg.se.phase10.Phase10Module;
 import de.htwg.se.phase10.aview.tui.TextUI;
 import de.htwg.se.phase10.controller.IPhase10Controller;
+import de.htwg.se.phase10.model.impl.Card;
 import junit.framework.TestCase;
 
 public class TextUITest extends TestCase{
@@ -34,6 +35,28 @@ public class TextUITest extends TestCase{
 		tui.inputLine("f");
 		tui.inputLine("c2");
 
+		tui.inputLine("f");
+		tui.inputLine("s");
+		controller.setCheckPhase();
+		/*controller.dropCardStack(0);
+		controller.dropCardStack(1);
+		controller.dropCardStack(2);
+		controller.dropCardStack(3);
+		controller.dropCardStack(4);
+		controller.dropCardStack(5);
+		controller.dropCardStack(6);
+		Card c1 = new Card(Card.Rank.EIGHT, Card.Color.BLUE, 0);
+		Card c2 = new Card(Card.Rank.EIGHT, Card.Color.BLUE, 0);
+		Card c3 = new Card(Card.Rank.EIGHT, Card.Color.RED, 0);
+		Card c4 = new Card(Card.Rank.ELEVEN, Card.Color.BLUE,0);
+		Card c5 = new Card(Card.Rank.ELEVEN, Card.Color.YELLOW,0);
+		Card c6 = new Card(Card.Rank.ELEVEN, Card.Color.GREEN,0);*/
+		
+		tui.inputLine("a1");
+		tui.inputLine("d");
+		tui.inputLine("s");
+		tui.inputLine("y");
+		tui.inputLine("q");
 		
 	}
 }
